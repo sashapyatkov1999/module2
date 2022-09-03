37c1305775cb4cbde127ea30d1728e68d92b79d1
skidrow <skidrow1999@bk.ru>
2022-09-03 12:15:41 +0300

    2 дз

--- src/Homework2/Homework2.java
+++ src/Homework2/Homework2.java
@@ -1,26 +1,26 @@
 package Homework2;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static sun.jvm.hotspot.HelloWorld.e;
+
 
 public class Homework2 {
     public static void main(String[] args) {
-        //Дан лист
-//        List<Integer> integers = List.of(1,2,3,4,5,5,8,8,9);
-//        Random random = new Random(1);
-//        integers.stream()
-//                .filter(integer -> integer > 4)
-//                .distinct()
-//                .map(integer -> Stream.generate(() -> random.nextInt(10))
-//                        .limit(integer)
-//                        .collect(Collectors.toList()))
-////                .
-//                .forEach(System.out::println);
+       // Дан лист
+        List<Integer> integers = List.of(1,2,3,4,5,5,8,8,9);
+        Random random = new Random(1);
+        integers.stream()
+                .filter(integer -> integer > 4)
+                .distinct()
+                .map(integer -> Stream.generate(() -> random.nextInt(10))
+                        .limit(integer)
+                        .collect(Collectors.toList()))
+                .flatMap(integers1 -> integers1.stream())
+                .map(integer -> integer * 10)
+                .reduce(Integer::sum)
+                .ifPresentOrElse(System.out::println, () -> System.out.println(0));
 
         //Задача №1
         //Создать класс пользователей, каждый из которых будет иметь порядковый номер, и список с числами
@@ -42,18 +42,23 @@ public class Homework2 {
         // Ожидаемый результат вывода на экран:
         // 1120 - из примера, 0 - при пустом листе
 
-        List<List<Integer>> lists = List.of(List.of(1, 2), List.of(3, 4, 5), List.of());
+        List<List<Integer>> lists = List.of(List.of(1, 2), List.of(3, 4, 5), List.of(1));
         //Задача №2
         // 1. Отсортировать список lists так, чтобы сначала были самые большие списки по размеру
         // 2. Вывести на экран все элементы
         // Ожидаемый результат: 3,4,5,1,2
         lists.stream()
-                .sorted(List::lastIndexOf)
-                .sorted((o1, o2 , o3) -> (). )
-                .forEach(System.out::println);
+                .sorted((o1, o2) -> (o2.size() - o1.size()))
+                .flatMap(Collection::stream)
+                .forEach(System.out::print);
         //Задача №3
         // 1. Узнать, есть ли в lists хотя бы один список, который содержит сумму всех элементов вложенного листа
         // равную 12
+        System.out.println("");
+         boolean anyMatch  = lists.stream()
+                 .map(e -> e.stream().reduce(Integer::sum).orElse(0))
+                 .anyMatch(e -> e == 12);
+        System.out.println(anyMatch);
 
     }
 }
